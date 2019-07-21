4df644f71f8f5aad4d7c92fd21222e3f8f6d31ea
le cong hieu <leconghieu1998@gmail.com>
2019-07-21 09:32:54 +0700

    variadic function!

--- variadic_function.c
+++ variadic_function.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+
 int add( int num_element, ...){
     va_list ap;
     int sum = 0;
@@ -14,7 +15,28 @@ int add( int num_element, ...){
     return sum;
 }
 
+float func_base(int a, int b){
+    return a*b;
+}
+
+typedef struct{
+    int a;
+    int b;
+} func_arg;
+
+int var_func(func_arg in){
+    int a = in.a? in.a : 10;
+    int b = in.b? in.b : -1;
+    return func_base(a, b);
+}
+
+#define func(...) var_func((func_arg){__VA_ARGS__})
+
 int main(){
-    printf("%d\n", add(4, 1, 2, 2, 2));
+    printf("%d\n", add(4, 1, 2, 2, 2));//7
+    printf("%d\n", func(3, 4));//3*4 = 12
+    printf("%d\n", func(4));//4*-1 = -4
+    printf("%d\n", func(0, 4));//10*4 = 40
+    printf("%d\n", func());//10*-1 = -10
     return 0;
 }
