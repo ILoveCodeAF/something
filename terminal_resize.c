0101f2596457ea2adb2ef3eb3dc846d8a8e4f41b
hieule <leconghieu1998@gmail.com>
2019-09-24 11:24:14 +0700

    handle window(terminal)_resize event compile by g++

--- terminal_resize.c
+++ terminal_resize.c
@@ -2,7 +2,7 @@
 #include <sys/ioctl.h>
 #include <signal.h>
 
-void sighandler(){
+void sighandler(int){
 	struct winsize ws;
 	ioctl(fileno(stdout), TIOCGWINSZ, &ws);
 	printf("terminal size: w = %d | h = %d\n", ws.ws_col, ws.ws_row);
@@ -10,7 +10,7 @@ void sighandler(){
 
 int main(){
 	int n;
-	signal(SIGWINCH, sighandler);
+	signal(SIGWINCH, &sighandler);
 	scanf("%d", &n);
 	return 0;
 }
