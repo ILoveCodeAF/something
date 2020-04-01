0a18ab9c81a4875defd0004bca0d4e1cab5c63e8
hieule <leconghieu1998@gmail.com>
2020-04-01 15:08:28 +0700

    use rapidly sort to test ex performances

--- performance.h
+++ performance.h
@@ -1,3 +1,13 @@
+#ifndef __PERFORMANCE_H_INCLUDED__
+#define __PERFORMANCE_H_INCLUDED__
+
+class Solution {
+	public:
+		int solution(int arr[], int n);
+	private:
+};
+
+#endif
 //Kate was given a birthday gift of three theater tickets. Now she is browsing the theater program for the next N days. On the program, performances are named by integers. Every day, one performance is staged. Kate wants to choose three days (not necessarily consecutive) to go to the theater.
 //In how many ways can she use her tickets? Two ways are different if the sequences of watched performances are different. Kate likes the theater, so she may watch one performance more than once. For example, if N = 4 and theater program looks as following: [1, 2, 1, 1], Kate has four possibilities to choose the dates: [1, 2, 1, 1], [1, 2, 1, 1], [1, 2, 1, 1], and [1, 2, 1, 1], but they create only three different sequences: (1, 2, 1), (1, 1, 1) and (2, 1, 1). The correct answer for this example is 3. Notice that the order of performances matters, so the first and the last sequences are considered different.
 //Write a function:
@@ -11,58 +21,4 @@
 //	Given A = [2, 2, 1, 2, 2], the function should return 4. There are four ways to spend tickets: (1, 2, 2), (2, 1, 2), (2, 2, 1) and (2, 2, 2).
 //	Given A = [1, 2], the function should return 0. Kate cannot use all three tickets in only two days.
 //
-
-#include <iostream>
-#include <unordered_map>
-#include "performance.h"
-
-int Solution::solution(int arr[], int n){
-	std::unordered_map<int, int> wtf_umap;
-	int *single_digit_distinguish = new int[n+1];
-	int *two_digits_distinguish = new int[n+1];
-	single_digit_distinguish[n] = two_digits_distinguish[n] = 0;
-
-	std::unordered_map<int, int>::iterator got;
-	for (int i = n-1; i >= 0; i--){
-		got = wtf_umap.find(arr[i]);
-		single_digit_distinguish[i] = single_digit_distinguish[i+1];
-		if (got == wtf_umap.end()) {
-			single_digit_distinguish[i] += 1;
-		}
-		
-		two_digits_distinguish[i] = single_digit_distinguish[i+1] + two_digits_distinguish[i+1];
-		if (got != wtf_umap.end()){
-			two_digits_distinguish[i] -= single_digit_distinguish[got->second+1];
-		}
-	//if (got == wtf_umap.end()){
-	//	two_digits_distinguish[i] = single_digit_distinguish[i+1] + two_digits_distinguish[i+1];
-	//}
-	//else {
-	//	two_digits_distinguish[i] = single_digit_distinguish[i+1]-single_digit_distinguish[got->second] + two_digits_distinguish[i+1]
-	//}
-		wtf_umap[arr[i]] = i;
-	}
-
-	wtf_umap.clear();
-	int ret = 0;
-	for(int i = 0; i < n; i++){
-		got = wtf_umap.find(arr[i]);
-		if (got == wtf_umap.end()) {
-			ret = ret + two_digits_distinguish[i+1];
-		}
-		wtf_umap[arr[i]] = i;
-	}
-//std::cout<<"single: ";
-//for (int i = 0; i < n+1; ++i) {
-//	std::cout<<single_digit_distinguish[i]<<' ';
-//}
-//std::cout<<"\ntwo   : ";
-//for (int i = 0; i < n+1; ++i) {
-//	std::cout<<two_digits_distinguish[i]<<' ';
-//}
-//std::cout<<std::endl;
-	delete[] single_digit_distinguish;
-	delete[] two_digits_distinguish;
-	return ret;
-}
 
--- rapidly_sort.cpp
+++ rapidly_sort.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "performance.h"
+
+
+#define SSTR( x ) static_cast< std::ostringstream & >( \
+		( std::ostringstream() << std::dec << x ) ).str()
 
 enum class Input { input_file, file_1, file_2 };
 
@@ -11,16 +19,31 @@ struct File {
 	std::ofstream outfile;
 };
 
-void sort(std::string filename){
-	int n = 10;
-	File file[10];
+void sort(std::string filename, std::string output_filename = "outfile.txt"){
+	int n = 11;
+	File file[11];
 	for(int i = 0; i < n; ++i) {
-		file[i].filename1 = "temp_file_" + char(i+'0');
-		file[i].filename2 = "temp_file0_" + char(i+'0');
+		char c = i+'0';
+		file[i].filename1 = "temp_file_";
+		file[i].filename1 += c;
+		file[i].filename2 = "temp_file0_";
+		file[i].filename2 = file[i].filename2 + c;
+//		std::cout<<file[i].filename1<<"  "<<file[i].filename2<<std::endl;
 	}
 	Input in = Input::input_file;
-
-	for (int i = 2; i >= 0; --i) {
+	int index = 0;
+	int max_length = 0;
+	std::ifstream infile;
+	infile.open(filename);
+	if(infile.is_open()){
+		std::string line;
+		while(getline(infile, line)){
+			max_length = (max_length > line.length())? max_length:line.length();
+		}
+		infile.close();
+	}
+	
+	for (int j = max_length-1; j >= 0; --j) {
 		// open file
 		if (in != Input::file_2) {
 			for(int i = 0; i < n; ++i){
@@ -35,12 +58,16 @@ void sort(std::string filename){
 		}
 
 		if (in == Input::input_file) {
-			std::fstream infile;
+			std::ifstream infile;
 			infile.open(filename);
 			if(infile.is_open()){
 				std::string line;
 				while(getline(infile, line)){
-					file[line[i]].outfile << line << "\n";
+					if (j >= line.length() || line[j] < '0' || line[j] > '9')
+						index = 0;
+					else
+						index = line[j] - '0' + 1;
+					file[index].outfile << line << "\n";
 				}
 				infile.close();
 			}
@@ -49,13 +76,19 @@ void sort(std::string filename){
 			if(file[i].infile.is_open()){
 				std::string line;
 				while(getline(file[i].infile, line)){
-					file[line[i]].outfile << line << "\n";
+					if (j >= line.length() || line[j] < '0' || line[j] > '9')
+						index = 0;
+					else
+						index = line[j] - '0' + 1;
+					file[index].outfile << line << "\n";
 				}
 			}
 		}
 		for(int i = 0; i < n; ++i){
-			file[i].infile.close();
-			file[i].outfile.close();
+			if(file[i].infile.is_open())
+				file[i].infile.close();
+			if(file[i].outfile.is_open())
+				file[i].outfile.close();
 		}
 		if(in != Input::file_2){
 			in = Input::file_2;
@@ -66,15 +99,14 @@ void sort(std::string filename){
 	if (in != Input::file_2) {
 		for(int i = 0; i < n; ++i){
 			file[i].infile.open(file[i].filename1);
-			file[i].outfile.open(file[i].filename2);
 		}
 	} else {
 		for(int i = 0; i < n; ++i){
 			file[i].infile.open(file[i].filename2);
-			file[i].outfile.open(file[i].filename1);
 		}
 	}
-	std::ofstream out_file(filename);
+	
+	std::ofstream out_file(output_filename);
 	
 	for(int i = 0; i < n; ++i){
 		if(file[i].infile.is_open()){
@@ -87,19 +119,74 @@ void sort(std::string filename){
 	out_file.close();
 	
 	for(int i = 0; i < n; ++i){
+		if (file[i].infile.is_open())
 			file[i].infile.close();
-			file[i].outfile.close();
-		}
+	}
 	for(int i = 0; i < n; ++i){
 		remove(file[i].filename1.c_str());
 		remove(file[i].filename2.c_str());
 	}
 }
+
+int count(std::string filename) {
+	std::string outfile = "output.dat";
+	sort(filename, outfile);
+
+	std::string line;
+	std::string cmp = "";
+	std::ifstream infile(outfile);
+	int ret = 0;
+	if(infile.is_open()){
+		while(getline(infile, line)){
+			if ( line != cmp ) {
+				cmp = line;
+				ret++;
+			}
+		}
+	}
+	infile.close();
+	remove(outfile.c_str());
+	return ret;
+}
+
+//// variadic template
+//template < typename... Args >
+//std::string sstr( Args &&... args )
+//{
+//	std::ostringstream sstr;
+//	// fold expression
+//	( sstr << std::dec << ... << args );
+//	return sstr.str();
+//}
 int main() {
+//std::string s = SSTR(1 << 3 << 4);
+//std::cout<<s<<std::endl;
+	int n = 200;
+	int arr[200];
+	srand(time(NULL));
+	for(int i = 0; i < n; ++i) {
+		arr[i] = rand() % (n/4) + 1;
+	}
+	
 	std::string filename = "input.txt";
 	std::ofstream outfile(filename);
-	outfile << "132\n" << "155\n";
+	for(int i = 0; i < n-2; ++i)
+		for(int j = i+1; j < n-1; ++j)
+			for(int k = j+1; k < n; ++k)
+				outfile << SSTR(arr[i] << " " << arr[j] << " " << arr[k]) <<std::endl;
 	outfile.close();
-	sort(filename);
+	
+//std::cout<<"read content in file: " << filename << std::endl;
+//std::ifstream infile(filename);
+//std::string line;
+//if(infile.is_open()){
+//	while(getline(infile, line)){
+//		std::cout<<line<<std::endl;
+//	}
+//}
+//infile.close();
+	std::cout<<"expected result: "<<count(filename)<<std::endl;
+	Solution s;
+	std::cout<<"my result: " << s.solution(arr, n) <<std::endl;
 	return 0;
 }
